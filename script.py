757b2bcb8412085d7ced1c4a61afd0ba961e9c67
ste <31259238+lnterger@users.noreply.github.com>
2022-04-24 21:46:22 -0700

    Centered Buttons

--- script.py
+++ script.py
@@ -23,17 +23,21 @@ def upload():
 
 #Creates boundary for the insides of program
 frm = ttk.Frame(window)
-frm['padding']=(150,150,150,150)
-frm.grid(row=11, columnspan=3, sticky=E+W+N+S, pady=5)
-frm.grid_columnconfigure(0, weight=1)
-frm.grid_rowconfigure(0, weight=1)
+frm['padding']=(150,150,0,0)
+frm.grid(rowspan=2, columnspan=3, sticky=E+W+N+S, pady=5)
+
+#Bottom Frame
+btmfrm = ttk.Frame(window)
+btmfrm.grid_columnconfigure(0, weight=1)
+btmfrm.grid_rowconfigure(0, weight=1)
+btmfrm.grid(rowspan=11, columnspan=3, sticky=E+W+N+S, padx=15)
 
 #Inside text of program
 ttk.Label(frm, text="Photo to ASCII Generator").grid(column=0, row=0)
-ttk.Label(frm, text="by ste, https://github.com/lnterger").grid(column=0, row=2)
+ttk.Label(frm, text="by ste, https://github.com/lnterger").grid(column=0, row=1)
 
-ttk.Button(frm, text="Upload Photo", command=upload).grid(column=1, row=3, pady=5)
-ttk.Button(frm, text="Quit", command=window.destroy).grid(column=3, row=3, pady=5)
+ttk.Button(btmfrm, text="Upload Photo", command=upload).grid(column=1, row=3, pady=5)
+ttk.Button(btmfrm, text="Quit", command=window.destroy).grid(column=3, row=3, pady=5)
 
 #Program doesn't shut down until window is destroyed by button
 window.mainloop()
